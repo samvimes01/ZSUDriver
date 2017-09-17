603f86fcd8cf6c2929e2762a90af6891f2f2aabe
SamVimes0 <k.aleksandr88@gmail.com>
2017-09-17 13:11:00 +0300

    first sight - 3 views - journal & add journal item

---
+++ app/src/main/java/ua/pp/myprojects/zsudriver/AddJournalItemActivity.java
@@ -0,0 +1,272 @@
+/*
+*    Copyright (C) 2017 Oleksandr Korneiko
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+package ua.pp.myprojects.zsudriver;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.Calendar;
+
+public class AddJournalItemActivity extends AppCompatActivity implements View.OnClickListener {
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+
+    private static final String TAG = "MainActivity";
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 1;
+    private static final int RC_PHOTO_PICKER = 2;
+
+
+    private ListView mMessageListView;
+    private JournalAdapter mJournalAdapter;
+
+    private EditText mDateEditText;
+    private EditText mNumberEditText;
+    private EditText mKmBeforeEditText;
+    private EditText mKmAfterEditText;
+    private EditText mFuelBeforeEditText;
+    private EditText mFuelAddEditText;
+    private EditText mFuelAfterEditText;
+    private TextView mFuelConsumption;
+    private TextView mKmDay;
+    private Button mSendButton;
+    static EditText mDatePeeker;
+
+    private String mUsername;
+
+    // Firebase instance variables
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessagesDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_journal_item);
+
+
+        mUsername = ANONYMOUS;
+
+        // Initialize Firebase components
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("milUnit").child("А4104").child("subUnits").child("medUnit").child("journal");
+
+
+        // Initialize references to views
+        mDatePeeker = (EditText) findViewById(R.id.trip_date);
+        mSendButton = (Button) findViewById(R.id.sendButton);
+
+        mDateEditText = (EditText) findViewById(R.id.trip_date);
+        mNumberEditText = (EditText) findViewById(R.id.waybill_number);
+        mKmBeforeEditText = (EditText) findViewById(R.id.input_km_before);
+        mKmAfterEditText = (EditText) findViewById(R.id.input_km_after);
+        mFuelBeforeEditText = (EditText) findViewById(R.id.input_fuel_before);
+        mFuelAddEditText = (EditText) findViewById(R.id.input_fuel_add);
+        mFuelAfterEditText = (EditText) findViewById(R.id.input_fuel_after);
+
+        mFuelConsumption = (TextView) findViewById(R.id.dayFuel);
+        mKmDay = (TextView) findViewById(R.id.kmDayView);
+
+        // DatePicker listener
+        mDatePeeker.setOnClickListener(this);
+
+        mSendButton.setOnClickListener(this);
+
+
+        mFuelBeforeEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+            }
+        });
+        mFuelAfterEditText.setOnClickListener(this);
+
+        TextWatcher inputTW = new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){
+
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Integer fuel = Integer.parseInt(mFuelAfterEditText.getText().toString()) - Integer.parseInt(mFuelBeforeEditText.getText().toString()) - Integer.parseInt(mFuelAddEditText.getText().toString());
+                Log.d("MyLog", fuel.toString());
+                mFuelConsumption.setText(fuel.toString());
+            }
+
+        };
+
+        mFuelAfterEditText.addTextChangedListener(inputTW);
+
+//        mFuelAfterEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View view, boolean b) {
+//                if (!b) {
+//                    Integer fuel = Integer.parseInt(mFuelAfterEditText.getText().toString()) - Integer.parseInt(mFuelBeforeEditText.getText().toString()) - Integer.parseInt(mFuelAddEditText.getText().toString());
+//                    Log.d("MyLog", fuel.toString());
+//                    mFuelConsumption.setText(fuel.toString());
+//                }
+//            }
+//        });
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    // User is signed out
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+    }
+
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
+        mJournalAdapter.clear();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch (view.getId()) {
+            case R.id.sendButton:
+                JournalItem journalItem = new JournalItem(mDateEditText.getText().toString(), Integer.parseInt(mNumberEditText.getText().toString()), Integer.parseInt(mKmBeforeEditText.getText().toString()), Integer.parseInt(mKmAfterEditText.getText().toString()), Integer.parseInt(mFuelBeforeEditText.getText().toString()), Integer.parseInt(mFuelAddEditText.getText().toString()), Integer.parseInt(mFuelAfterEditText.getText().toString()));
+                mMessagesDatabaseReference.push().setValue(journalItem);
+                finish();
+                break;
+            case R.id.trip_date:
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+                break;
+            case R.id.input_fuel_after:
+                Integer km = Integer.parseInt(mKmAfterEditText.getText().toString()) - Integer.parseInt(mKmBeforeEditText.getText().toString());
+                Log.d("MyLog", km.toString());
+                mKmDay.setText(km.toString());
+                break;
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            mDatePeeker.setText(day + "." + month + "." + year);
+        }
+    }
+}
--- app/src/main/java/ua/pp/myprojects/zsudriver/JournalActivity.java
+++ app/src/main/java/ua/pp/myprojects/zsudriver/JournalActivity.java
@@ -19,24 +19,31 @@ package ua.pp.myprojects.zsudriver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class JournalActivity extends AppCompatActivity implements View.OnClickListener {
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
 
@@ -47,8 +54,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int RC_PHOTO_PICKER = 2;
 
 
+    private ListView mMessageListView;
+    private JournalAdapter mJournalAdapter;
     private ProgressBar mProgressBar;
-    private Button mJournalButton;
+    private FloatingActionButton mAddJItemButton;
+
     private String mUsername;
 
     // Firebase instance variables
@@ -61,23 +71,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_journal);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(this);
 
         mUsername = ANONYMOUS;
+
         // Initialize Firebase components
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
 
+        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("milUnit").child("А4104").child("subUnits").child("medUnit").child("journal");
+        attachDatabaseReadListener();
 
         // Initialize references to views
-        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-        mJournalButton = (Button) findViewById(R.id.btnJournal);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar2);
+        mMessageListView = (ListView) findViewById(R.id.lstV_month);
+
+        mAddJItemButton = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        // Initialize message ListView and its adapter
+        List<JournalItem> journalItems = new ArrayList<>();
+        mJournalAdapter = new JournalAdapter(this, R.layout.item_message, journalItems);
+        mMessageListView.setAdapter(mJournalAdapter);
 
         // Initialize progress bar
         mProgressBar.setVisibility(ProgressBar.INVISIBLE);
 
-        mJournalButton.setOnClickListener(this);
+        mAddJItemButton.setOnClickListener(this);
+
 
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -120,6 +147,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (mAuthStateListener != null) {
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
         }
+        mJournalAdapter.clear();
+        detachDatabaseReadListener();
     }
 
     @Override
@@ -142,21 +171,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void onSignedInInitialize(String username) {
         mUsername = username;
+        attachDatabaseReadListener();
     }
 
     private void onSignedOutCleanup() {
         mUsername = ANONYMOUS;
+        mJournalAdapter.clear();
+        detachDatabaseReadListener();
+    }
+
+    private void attachDatabaseReadListener() {
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    JournalItem journalItem = dataSnapshot.getValue(JournalItem.class);
+                    mJournalAdapter.add(journalItem);
+                }
+
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {}
+                public void onChildRemoved(DataSnapshot dataSnapshot) {}
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {}
+                public void onCancelled(DatabaseError databaseError) {}
+            };
+            mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        }
     }
 
 
+    private void detachDatabaseReadListener() {
+        if (mChildEventListener != null) {
+            mMessagesDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
+    }
 
     @Override
     public void onClick(View view) {
         Intent intent;
 
         switch(view.getId()) {
-            case R.id.btnJournal:
-                intent = new Intent(this, JournalActivity.class);
+            case R.id.fab:
+                intent = new Intent(this, AddJournalItemActivity.class);
                 startActivity(intent);
                 break;
         }
